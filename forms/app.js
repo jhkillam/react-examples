8fdb6d7ab856bf4f89b6598d6ff1bb81f67d700f
Joey Killam <jhkillam@gmail.com>
2019-11-21 20:43:54 -0600

    testing react forms

--- forms/app.js
+++ forms/app.js
@@ -34,28 +34,56 @@ class FruitsFormWithState extends React.Component {
 class MyNameFormWithState extends React.Component {
   state = {
     firstName: "",
-    lastName: ""
+    lastName: "",
+    people: []
   }
-  handleFirstName = (changeEvent) => {
-    console.log(changeEvent.target.value)
+  handleInputChange = (changeEvent) => {
+    // console.log(changeEvent.target.value)
     this.setState({
-      firstName: changeEvent.target.value
+      [changeEvent.target.name]: changeEvent.target.value
+    })
+  }
+  handleFormSubmit = (submitEvent) => {
+    submitEvent.preventDefault()
+    const newPeople = [...this.state.people]
+    newPeople.push({
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
+    })
+    this.setState({
+      people: newPeople
     })
   }
   render() {
-    return (<div className="container">
-      <div className="form-group">
-        <label>First Name</label>
-        <input type="text" name="first-name"
-          onChange={this.handleFirstName}
-          value={this.state.firstName}/>
-      </div>
-      <div className="form-group">
-        <label>Last Name</label>
-        <input type="text" name="last-name"/>
-      </div>
-      <h1>This is the state</h1>
-      <pre><code>{JSON.stringify(this.state)}</code></pre>
+    return (
+    <div className="container">
+      <form onSubmit={this.handleFormSubmit}>
+        <div className="form-group">
+          <label>First Name</label>
+          <input type="text" name="firstName"
+            onChange={this.handleInputChange}
+            value={this.state.firstName}/>
+        </div>
+        <div className="form-group">
+          <label>Last Name</label>
+          <input type="text" name="lastName"
+            onChange={this.handleInputChange}
+            value={this.state.lastName}
+          />
+        </div>
+        <button type="submit">Save!</button>
+        </form>
+        <h1>This is the state</h1>
+        <pre><code>{JSON.stringify(this.state)}</code></pre>
+        <ul>
+          {this.state.people.map((person, index) => {
+            return (
+            <li key={index}>
+              {person.firstName} {person.lastName}
+            </li>
+            )})
+          }
+        </ul>
     </div>)
   }
 }
